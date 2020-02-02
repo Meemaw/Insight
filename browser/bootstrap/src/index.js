edd6019b3af9cbb7c6dbce33a59e8305476726d1
Matej Snuderl <matej.snuderl@cognite.com>
2020-02-02 16:36:14 +0100

    build step

--- browser/bootstrap/src/index.js
+++ browser/bootstrap/src/index.js
@@ -6,11 +6,11 @@ window._i_script = 'cdn.insight.com/s/is.js';
 window._i_org = '<ORG>';
 window._i_ns = 'IS';
 
-((doc, scriptTag) => {
+((window, doc, scriptTag) => {
   const scriptElement = doc.createElement(scriptTag);
   scriptElement.async = true;
   scriptElement.crossOrigin = 'anonymous';
   scriptElement.src = `https://${window._i_script}`;
   const firstScript = doc.getElementsByTagName(scriptTag)[0];
   firstScript.parentNode.insertBefore(scriptElement, firstScript);
-})(document, 'script');
+})(window, document, 'script');
