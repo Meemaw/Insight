76acfc148ea562c3964420b9e43890f4ccd4a4be
Matej Snuderl <matej.snuderl@cognite.com>
2020-02-02 14:46:00 +0100

    start of a simple tracking script

--- browser/tracking/src/index.spec.ts
+++
@@ -1,7 +0,0 @@
-import { sum } from 'index';
-
-describe('sum', () => {
-  it('should sum positive values', () => {
-    expect(sum(1, 2)).toEqual(3);
-  });
-});
--- browser/tracking/src/index.ts
+++ browser/tracking/src/index.ts
@@ -1,3 +1,20 @@
-export const sum = (a: number, b: number) => {
-  return a + b;
-};
+import Context from 'context';
+import EventQueue from 'queue';
+import { EventType } from 'event';
+
+(location => {
+  let { href: lastLocation } = location;
+  const context = new Context();
+  const eventQueue = new EventQueue(context);
+
+  const onNavigationChange = () => {
+    const { href: currentLocation } = location;
+    if (lastLocation !== currentLocation) {
+      lastLocation = currentLocation;
+      eventQueue.enqueue(EventType.NAVIGATE, [currentLocation, document.title]);
+    }
+  };
+
+  window.addEventListener('popstate', onNavigationChange);
+  // eslint-disable-next-line no-restricted-globals
+})(location);
