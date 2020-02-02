2116f210fb0e464602d3a6988172a6267128a458
Matej Snuderl <matej.snuderl@cognite.com>
2020-02-02 17:14:00 +0100

    upload gziped script

--- browser/tracking/rollup.config.js
+++ browser/tracking/rollup.config.js
@@ -1,12 +1,17 @@
 /* eslint-disable @typescript-eslint/camelcase */
 import { terser } from 'rollup-plugin-terser';
 import typescript from '@rollup/plugin-typescript';
+import gzipPlugin from 'rollup-plugin-gzip';
 
 const input = 'src/index.ts';
 
 export default {
   input,
-  plugins: [typescript(), terser({ compress: true, mangle: true })],
+  plugins: [
+    typescript(),
+    terser({ compress: true, mangle: true }),
+    gzipPlugin(),
+  ],
   output: {
     file: 'dist/insight.js',
   },
