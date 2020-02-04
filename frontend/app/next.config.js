4d3966b8065c12126ffdb9f9327cc8e149cb2b90
Matej Snuderl <matej.snuderl@cognite.com>
2020-02-04 20:42:13 +0100

    make build work

---
+++ frontend/app/next.config.js
@@ -0,0 +1,16 @@
+/* eslint-disable no-param-reassign */
+/* eslint-disable @typescript-eslint/no-var-requires */
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+
+module.exports = {
+  // Required for absolute imports in Next.js
+  webpack: (config, _options) => {
+    if (config.resolve.plugins) {
+      config.resolve.plugins.push(new TsconfigPathsPlugin());
+    } else {
+      config.resolve.plugins = [new TsconfigPathsPlugin()];
+    }
+
+    return config;
+  },
+};
