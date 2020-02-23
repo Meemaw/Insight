36a8d6b31dbcde1e2260f85db364f9f8b0f11abe
Matej Snuderl <matej.snuderl@cognite.com>
2020-02-23 15:09:49 +0100

    testcafe bug

---
+++ frontend/app/src/pages/testcafe-bug.tsx
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from 'react';
+import ky from 'ky-universal';
+
+type Comment = {
+  postId: number;
+  id: number;
+  name: string;
+  email: string;
+  body: string;
+};
+
+const TestcafeBug = () => {
+  const [comments, setComments] = useState<Comment[]>([]);
+
+  useEffect(() => {
+    ky.get('https://jsonplaceholder.typicode.com/comments')
+      .json<Comment[]>()
+      .then(setComments);
+  });
+
+  return (
+    <ul>
+      {comments.map(comm => (
+        <li key={comm.id}>{comm.body}</li>
+      ))}
+    </ul>
+  );
+};
+
+export default TestcafeBug;
