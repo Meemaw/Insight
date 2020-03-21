fdc49efa1643b50ae56b20fb48d52a7874dfecf8
Matej Snuderl <matej.snuderl@cognite.com>
2020-03-21 14:42:06 +0100

    run migrations

--- backend/auth-service/migrations/scripts/flyway.sh
+++ backend/auth-service/migrations/scripts/flyway.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
 DATABASE_HOST=${DATABASE_HOST:="localhost"}
+DATABASE_PASSWORD=${DATABASE_PASSWORD:="postgres"}
+DATABASE_USER=${DATABASE_USER:="postgres"}
+
 JDBC_URL="jdbc:postgresql://${DATABASE_HOST}/${DATABASE_NAME}"
 
 echo "Starting flyway ${JDBC_URL}" 
