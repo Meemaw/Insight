152b92c8fcd4f6708010ade39b2eb6f934944145
Matej Snuderl <ematej.snuderl@gmail.com>
2020-05-11 21:30:26 +0200

    chore: implement AbstractApiTestContainer

--- backend/shared/testing-utils/src/main/java/com/meemaw/test/testconainers/api/Api.java
+++ backend/shared/testing-utils/src/main/java/com/meemaw/test/testconainers/api/Api.java
@@ -1,23 +1,31 @@
 package com.meemaw.test.testconainers.api;
 
 import com.meemaw.test.project.ProjectUtils;
+import com.meemaw.test.testconainers.pg.PostgresTestExtension;
 import java.nio.file.Path;
-import java.util.Optional;
+import java.util.Collection;
+import java.util.List;
+import org.testcontainers.containers.GenericContainer;
 
 public enum Api {
   AUTH {
     @Override
-    public Path dockerfile() {
-      return ProjectUtils.getFromBackend("auth", "api", "docker", "Dockerfile.jvm");
-    }
-
-    @Override
-    public Optional<Path> migrations() {
-      return Optional.of(ProjectUtils.getFromBackend("auth", "api", "migrations"));
+    public Collection<GenericContainer<?>> dependencies() {
+      return List.of(PostgresTestExtension.getInstance());
     }
   };
 
-  public abstract Path dockerfile();
+  public String imageName() {
+    return String.format("%s-test-api", name().toLowerCase());
+  }
 
-  public abstract Optional<Path> migrations();
+  public Path dockerfile() {
+    return ProjectUtils.getFromBackend(name().toLowerCase(), "api", "docker", "Dockerfile.jvm");
+  }
+
+  public Path migrations() {
+    return ProjectUtils.getFromBackend(name().toLowerCase(), "api", "migrations", "sql");
+  }
+
+  public abstract Collection<GenericContainer<?>> dependencies();
 }
--- backend/shared/testing-utils/src/main/java/com/meemaw/test/testconainers/api/auth/AuthApiTestContainer.java
+++ backend/shared/testing-utils/src/main/java/com/meemaw/test/testconainers/api/auth/AuthApiTestContainer.java
@@ -1,88 +1,19 @@
 package com.meemaw.test.testconainers.api.auth;
 
-import com.meemaw.test.project.ProjectUtils;
+import com.meemaw.test.testconainers.api.AbstractApiTestContainer;
 import com.meemaw.test.testconainers.api.Api;
-import com.meemaw.test.testconainers.pg.PostgresTestExtension;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.nio.file.Path;
 import lombok.extern.slf4j.Slf4j;
-import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.output.Slf4jLogConsumer;
-import org.testcontainers.containers.wait.strategy.Wait;
 
 @Slf4j
-public class AuthApiTestContainer extends GenericContainer<AuthApiTestContainer> {
-
-  private static final int PORT = 8080;
+public class AuthApiTestContainer extends AbstractApiTestContainer<AuthApiTestContainer> {
 
   private AuthApiTestContainer() {
-    super(dockerImage());
-  }
-
-  @Override
-  public void start() {
-    PostgresTestExtension.start();
-    PostgresTestExtension.getInstance().applyMigrations(Api.AUTH.migrations().orElseThrow());
-    withEnv("POSTGRES_HOST", PostgresTestExtension.getInstance().getHost());
-    super.start();
+    super(Api.AUTH);
   }
 
   /** @return */
   public static AuthApiTestContainer newInstance() {
-    return new AuthApiTestContainer()
-        .withExposedPorts(PORT)
-        .withLogConsumer(new Slf4jLogConsumer(log))
-        .waitingFor(Wait.forHttp("/health").forStatusCode(200));
-  }
-
-  // TODO: use testcontainers for this
-  private static String dockerImage() {
-    Path dockerfile = Api.AUTH.dockerfile();
-    Path context = ProjectUtils.backendPath();
-    String imageName = "auth-api-test";
-
-    log.info(
-        "Building auth api dockerfile={} context={} imageName={}",
-        dockerfile.toString(),
-        context.toAbsolutePath(),
-        imageName);
-
-    ProcessBuilder builder =
-        new ProcessBuilder(
-            "docker",
-            "build",
-            "-f",
-            dockerfile.toString(),
-            "-t",
-            imageName,
-            context.toAbsolutePath().toString());
-    builder.redirectErrorStream(true);
-    Process p;
-    try {
-      p = builder.start();
-
-      BufferedReader r = new BufferedReader(new InputStreamReader(p.getInputStream()));
-      String line;
-      while (true) {
-        line = r.readLine();
-        if (line == null) {
-          break;
-        }
-        log.info(line);
-      }
-
-      if (p.waitFor() > 0) {
-        throw new Exception("failed to build auth api");
-      }
-
-      return imageName;
-    } catch (Exception ex) {
-      throw new RuntimeException(ex);
-    }
-  }
-
-  public int getPort() {
-    return getMappedPort(PORT);
+    return new AuthApiTestContainer().withLogConsumer(new Slf4jLogConsumer(log));
   }
 }
