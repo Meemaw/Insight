bd3d661f78746b43239a979180b86502232e033e
Matej Snuderl <ematej.snuderl@gmail.com>
2020-05-19 14:56:19 +0200

    fix: use lazy instantiation

--- backend/auth/auth-api/src/main/java/com/meemaw/auth/sso/datasource/HazelcastProvider.java
+++ backend/auth/auth-api/src/main/java/com/meemaw/auth/sso/datasource/HazelcastProvider.java
@@ -4,21 +4,21 @@ import com.hazelcast.config.XmlConfigBuilder;
 import com.hazelcast.core.Hazelcast;
 import com.hazelcast.core.HazelcastInstance;
 import io.quarkus.runtime.ShutdownEvent;
-import io.quarkus.runtime.StartupEvent;
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.event.Observes;
-import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @ApplicationScoped
 public class HazelcastProvider {
 
-  @Getter private HazelcastInstance instance;
+  private HazelcastInstance instance;
 
-  public void init(@Observes StartupEvent event) {
-    log.info("Initializing ...");
-    instance = Hazelcast.newHazelcastInstance(new XmlConfigBuilder().build());
+  public HazelcastInstance getInstance() {
+    if (instance == null) {
+      instance = Hazelcast.newHazelcastInstance(new XmlConfigBuilder().build());
+    }
+    return instance;
   }
 
   public void shutdown(@Observes ShutdownEvent event) {
