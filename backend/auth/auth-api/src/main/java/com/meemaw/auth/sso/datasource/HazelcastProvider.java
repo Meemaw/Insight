27610910a47967137529b977ae57fbe73af15eab
Matej Snuderl <ematej.snuderl@gmail.com>
2020-05-19 12:21:45 +0200

    fix: @Observes StartupEvent event

--- backend/auth/auth-api/src/main/java/com/meemaw/auth/sso/datasource/HazelcastProvider.java
+++ backend/auth/auth-api/src/main/java/com/meemaw/auth/sso/datasource/HazelcastProvider.java
@@ -4,19 +4,27 @@ import com.hazelcast.config.Config;
 import com.hazelcast.config.XmlConfigBuilder;
 import com.hazelcast.core.Hazelcast;
 import com.hazelcast.core.HazelcastInstance;
+import io.quarkus.runtime.ShutdownEvent;
+import io.quarkus.runtime.StartupEvent;
 import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.event.Observes;
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @ApplicationScoped
 public class HazelcastProvider {
 
-  private static HazelcastInstance hazelcastInstance;
+  @Getter private HazelcastInstance hazelcastInstance;
 
-  public HazelcastInstance getInstance() {
-    if (hazelcastInstance == null) {
-      Config hazConfig = new XmlConfigBuilder().build();
-      hazelcastInstance = Hazelcast.newHazelcastInstance(hazConfig);
-    }
-    return hazelcastInstance;
+  public void init(@Observes StartupEvent event) {
+    log.info("Initializing ...");
+    Config hazConfig = new XmlConfigBuilder().build();
+    hazelcastInstance = Hazelcast.newHazelcastInstance(hazConfig);
   }
 
+  public void shutdown(@Observes ShutdownEvent event) {
+    log.info("Shutting down ...");
+    hazelcastInstance.shutdown();
+  }
 }
