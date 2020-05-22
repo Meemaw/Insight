a6ad5c9bf176b9982ac9aee2c238f91e1777a10f
Blaž Šnuderl <snuderl@gmail.com>
2020-05-22 22:01:50 +0200

    Add search-indexer and es

--- backend/events/search-indexer/src/main/java/com/meemaw/search/indexer/KafkaUtils.java
+++ backend/events/search-indexer/src/main/java/com/meemaw/search/indexer/KafkaUtils.java
@@ -6,8 +6,8 @@ import lombok.experimental.UtilityClass;
 @UtilityClass
 public class KafkaUtils {
 
-  private final String BOOTSTRAP_SERVERS = "BOOTSTRAP_SERVERS";
-  private final String DEFAULT_BOOTSTRAP_SERVERS = "localhost:9092";
+  private final String BOOTSTRAP_SERVERS = "KAFKA_BOOTSTRAP_SERVERS";
+  private final String DEFAULT_BOOTSTRAP_SERVERS = "PLAINTEXT://kafka:9092";
 
   public String fromEnvironment(String name) {
     return Optional.ofNullable(System.getenv(name)).orElse(DEFAULT_BOOTSTRAP_SERVERS);
