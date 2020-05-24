b69b5c42c316279339dbf35a982e7fd1598e5518
Blaž Šnuderl <snuderl@gmail.com>
2020-05-24 20:54:45 +0200

    fix

--- backend/events/search-indexer/src/main/java/com/meemaw/search/indexer/KafkaUtils.java
+++ backend/events/search-indexer/src/main/java/com/meemaw/search/indexer/KafkaUtils.java
@@ -7,7 +7,7 @@ import lombok.experimental.UtilityClass;
 public class KafkaUtils {
 
   private final String BOOTSTRAP_SERVERS = "KAFKA_BOOTSTRAP_SERVERS";
-  private final String DEFAULT_BOOTSTRAP_SERVERS = "PLAINTEXT://kafka:9092";
+  private final String DEFAULT_BOOTSTRAP_SERVERS = "localhost:9092";
 
   public String fromEnvironment(String name) {
     return Optional.ofNullable(System.getenv(name)).orElse(DEFAULT_BOOTSTRAP_SERVERS);
