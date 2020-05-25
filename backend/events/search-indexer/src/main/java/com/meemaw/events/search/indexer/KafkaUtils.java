250c45549d9ad4b9cc8fad35731cd730d0459bd3
Matej Snuderl <ematej.snuderl@gmail.com>
2020-05-25 13:53:41 +0200

    fix: fields before anything

--- backend/events/search-indexer/src/main/java/com/meemaw/events/search/indexer/KafkaUtils.java
+++ backend/events/search-indexer/src/main/java/com/meemaw/events/search/indexer/KafkaUtils.java
@@ -4,10 +4,10 @@ import java.util.Optional;
 
 public final class KafkaUtils {
 
-  private KafkaUtils() {}
-
   private static final String BOOTSTRAP_SERVERS = "KAFKA_BOOTSTRAP_SERVERS";
   private static final String DEFAULT_BOOTSTRAP_SERVERS = "localhost:9092";
+
+  private KafkaUtils() {}
 
   public static String fromEnvironment(String name) {
     return Optional.ofNullable(System.getenv(name)).orElse(DEFAULT_BOOTSTRAP_SERVERS);
