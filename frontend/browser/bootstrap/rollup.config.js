e6d7a4c6ff6ca472e711770063732254a5bd0559
Matej Snuderl <ematej.snuderl@gmail.com>
2020-05-27 15:17:51 +0200

    feat: build bootstrap scripts

--- frontend/browser/bootstrap/rollup.config.js
+++ frontend/browser/bootstrap/rollup.config.js
@@ -3,26 +3,67 @@ import path from 'path';
 
 import { terser } from 'rollup-plugin-terser';
 import gzipPlugin from 'rollup-plugin-gzip';
+import replace from '@rollup/plugin-replace';
 
 const prettier = require('rollup-plugin-prettier');
 
 const input = path.join('src', 'index.js');
-const output = path.join('dist', 'insight.js');
+const outputBaseName = 'insight.js';
 
-const plugins = [terser({ compress: false, mangle: true }), prettier()];
+const basePlugins = [terser({ compress: false, mangle: true }), prettier()];
 
-export default [
-  {
+const environments = {
+  local: 'https://d1l87tz7sw1x04.cloudfront.net/s/local.insight.js',
+  development: 'https://d1l87tz7sw1x04.cloudfront.net/s/development.insight.js',
+  production: 'https://d1l87tz7sw1x04.cloudfront.net/s/insight.js',
+};
+
+const envConfig = (env) => {
+  const baseName = outputBaseName;
+  const fileName = `${env}.${baseName}`;
+  const trackingScript = environments[env];
+
+  switch (env) {
+    case 'production':
+      return { trackingScript, fileName: baseName };
+    default:
+      return { trackingScript, fileName };
+  }
+};
+
+const config = (env) => {
+  const { fileName, trackingScript } = envConfig(env);
+  const output = path.join('dist', fileName);
+
+  const plugins = [
+    ...basePlugins,
+    replace({
+      'process.env.TRACKING_SCRIPT': JSON.stringify(trackingScript),
+    }),
+  ];
+
+  return {
     input,
     plugins: [...plugins, gzipPlugin()],
     output: {
       file: output,
     },
-  },
+  };
+};
+
+export default [
+  ...Object.keys(environments).map((env) => config(env)),
   {
     input,
     plugins: [
-      ...plugins,
+      ...[
+        ...basePlugins,
+        replace({
+          'process.env.TRACKING_SCRIPT': JSON.stringify(
+            environments.development
+          ),
+        }),
+      ],
       {
         name: 'add-script-tag',
         renderChunk: (source, _chunkInfo, _outputOptions) => {
@@ -31,7 +72,7 @@ export default [
       },
     ],
     output: {
-      file: output.replace('.js', '.html'),
+      file: path.join('dist', outputBaseName.replace('.js', '.html')),
     },
   },
 ];
