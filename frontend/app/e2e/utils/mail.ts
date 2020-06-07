512923a69592a8018b1148f75020f2e550797a42
Matej Snuderl <ematej.snuderl@gmail.com>
2020-06-07 19:04:17 +0200

    read from artifacts path?

--- frontend/app/e2e/utils/mail.ts
+++ frontend/app/e2e/utils/mail.ts
@@ -4,7 +4,10 @@ import { readFileSync } from 'fs';
 const linkPattern = /.*href="(http:\/\/.*)".*$/;
 
 export const findLinkFromDockerLog = () => {
-  const dockerLogFile = join(process.cwd(), '..', '..', 'docker.log');
+  const basePath =
+    process.env.ARTIFACTS_PATH || join(process.cwd(), '..', '..');
+
+  const dockerLogFile = join(basePath, 'docker.log');
   const lines = String(readFileSync(dockerLogFile)).split('\n');
 
   return lines.reduce((maybeLink, line) => {
